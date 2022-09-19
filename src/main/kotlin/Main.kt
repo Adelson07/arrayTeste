d2c0e405d5a0ad594956ba2a5c13badeb820eb70
adels <adelson.sula.jesus@hotmail.com>
2022-09-19 20:21:48 -0300

    First commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main (args: Array(String)){
+        val cotacaoDolarArray: Array(Double)
+
+        cotacaoDolarArray = arrayOf(5.12, 4.80, 4.60, 5.20, 3.50, 5.00)
+        val maior = cotacaoDolarArray[0]
+        val menor = cotacaoDolarArray[0]
+        val lucro = 0.0
+}
